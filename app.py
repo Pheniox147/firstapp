6589bec5cfc0a5b91b5055bcd364d3cb9c0a9ff1
VARIKUTI YASASRI <80969337+Pheniox147@users.noreply.github.com>
2023-02-27 13:16:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.title("My First Streamlit App") 
+
+btn_click = st.button("click me! :ok_hand:")
+
+if btn_click==True:
+    st.subheader("lets connect on linkdeln :wave: https://www.linkedin.com/in/varikuti-yasasri-01422420b ")
+    st.balloons()
